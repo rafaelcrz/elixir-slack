88a742403d8922eaec21d1ef8c5725f6b6ead164
Rafael Felipe <rafael.felipe1989@gmail.com>
2018-04-13 00:20:25 -0300

    Auth verify token tests

--- test/elixir_slack_test.exs
+++ test/elixir_slack_test.exs
@@ -1,8 +1,29 @@
 defmodule ElixirSlackTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest ElixirSlack
+  alias ElixirSlack.Api.Auth
 
   test "greets the world" do
     assert ElixirSlack.hello() == :world
+  end
+
+  # @tag :check_valid_token
+  # test "when verify a valid token return a {:ok, content}" do
+  #   token = "xoxp-put_a_valid_token"
+  #   assert {:ok, _content} = Auth.verify(token)
+  # end
+
+  @tag :check_invalid_token
+  test "when verify an invalid token return a {:error, reason}" do
+    token = "xoxp-912912u92-121212-12121-12-0000"
+    assert {:error, _reason} = Auth.verify(token)
+  end
+
+  @tag :check_nil_token
+  test "when get a nil token value return a {:error, 'authentication token is missing'}" do
+    token = nil
+    assert {:error, "authentication token is missing"} == Auth.verify(token)
+    token = ""
+    assert {:error, "authentication token is missing1"} == Auth.verify(token)
   end
 end
