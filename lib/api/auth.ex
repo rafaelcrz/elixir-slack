897cfa34963a97e1335bdc87298aa71f9799e491
Rafael Felipe <rafael.felipe1989@gmail.com>
2018-04-15 11:11:39 -0300

    refactor get token from env not params

--- lib/api/auth.ex
+++ lib/api/auth.ex
@@ -7,10 +7,6 @@ defmodule ElixirSlack.Auth do
 
   @slack_host Application.get_env(:elixir_slack, :slack_host)
 
-  defp header(auth_token) do
-    [{"Authorization", "Bearer #{auth_token}"}]
-  end
-
   @doc """
   Checks authentication & identity.
   This method checks authentication and tells "you" who you are, even if you might be a bot.
@@ -18,7 +14,7 @@ defmodule ElixirSlack.Auth do
 
   ## Examples
 
-    iex> ElixirSlack.Api.Auth.verify("valid_token")
+    iex> ElixirSlack.Api.Auth.verify()
       {:ok, %{
         "ok" => true,
         "team" => "your team name",
@@ -27,22 +23,10 @@ defmodule ElixirSlack.Auth do
         "user" => "your_bot_name",
         "user_id" => "your_user_id"
       }}
-
-    iex> ElixirSlack.Api.Auth.verify("invalid_token")
-      {:error, reason}
-
-    iex> ElixirSlack.Api.Auth.verify(nil)
-        {:error, "authentication token is missing"}
-
-    iex> ElixirSlack.Api.Auth.verify("")
-        {:error, "authentication token is missing"}
   """
-  @spec verify(auth_token :: binary) :: {atom, binary}
-  def verify(nil), do: {:error, "authentication token is missing"}
-  def verify(""), do: {:error, "authentication token is missing"}
-
-  def verify(auth_token) do
-    Client.post(@slack_host<>"/auth.test", %{}, header(auth_token))
+  @spec verify() :: {atom, binary}
+  def verify() do
+    Client.post(@slack_host<>"/auth.test", %{}, [])
     |> Client.process_response()
   end
 end
