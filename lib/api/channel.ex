8c66967c4bdd58239bb99e3af0418b5cebdbe100
Rafael Felipe <rafael.felipe1989@gmail.com>
2018-04-15 15:18:21 -0300

    get all channels on slack team

--- lib/api/channel.ex
+++ lib/api/channel.ex
@@ -51,4 +51,50 @@ defmodule ElixirSlack.Channel do
     |> Client.process_response()
   end
 
+  @doc """
+  Gets all channels on slack team
+
+    ## Examples
+
+    iex> ElixirSlack.Channel.info()
+      %{
+      "channels" => [
+        %{
+          "created" => 1523330538,
+          "creator" => "UA4RAAE",
+          "id" => "UA4RAAESD",
+          "is_archived" => false,
+          "is_channel" => true,
+          "is_general" => true,
+          "is_member" => false,
+          "is_mpim" => false,
+          "is_org_shared" => false,
+          "is_private" => false,
+          "is_shared" => false,
+          "members" => ["UA4RAAEWF"],
+          "name" => "general",
+          "name_normalized" => "general",
+          "num_members" => 1,
+          "previous_names" => [],
+          "purpose" => %{
+            "creator" => "UA4RAAE",
+            "last_set" => 1523330538,
+            "value" => "This channel is for workspace-wide communication and announcements. All members are in this channel."
+          },
+          "topic" => %{
+            "creator" => "UA4RAAE",
+            "last_set" => 1523330538,
+            "value" => "Company-wide announcements and work-based matters"
+          },
+          "unlinked" => 0
+        }
+      ]
+    }
+  """
+  @spec list() :: {atom, map}
+  def list() do
+    Client.get(@slack_host<>"/channels.list",[], params: %{})
+    |> Client.process_response()
+  end
+
 end
